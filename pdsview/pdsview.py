2c5bf42f342b395891ce643bd369728451fb8a1b
Zach Burnham <zachary.burnham@asu.edu>
2015-07-21 12:27:59 -0700

    Added the ability to open multiple images simultaneously. Now also supports the ability to open images as arguments when calling pdsview.

--- pdsview/pdsview.py
+++ pdsview/pdsview.py
@@ -1,14 +1,7 @@
 #! /usr/bin/env python
-#
-# example1_qt.py -- Simple, configurable FITS viewer.
-#
-# Eric Jeschke (eric@naoj.org)
-#
-# Copyright (c)  Eric R. Jeschke.  All rights reserved.
-# This is open-source software licensed under a BSD license.
-# Please see the file LICENSE.txt for details.
-#
+
 import sys
+import os
 # import logging
 import PDSImage
 import label
@@ -17,6 +10,10 @@ from ginga.qtw.QtHelp import QtGui, QtCore
 from ginga.qtw.ImageViewCanvasQt import ImageViewCanvas
 
 STD_FORMAT = '%(asctime)s | %(levelname)1.1s | %(filename)s:%(lineno)d (%(funcName)s) | %(message)s'
+#
+#
+# class PDSModel():
+#
 
 
 class PDSViewer(QtGui.QMainWindow):
@@ -25,6 +22,9 @@ class PDSViewer(QtGui.QMainWindow):
         super(PDSViewer, self).__init__()
 
         self.image = ""
+        self.index_count = 0
+        self.pos = 1
+        self.names = []
 
         # Set the subwindow names here. This implementation will help prevent
         # the main window from spawning duplicate children. Even if the
@@ -59,6 +59,12 @@ class PDSViewer(QtGui.QMainWindow):
         # toggle is found in load_file().
         open_file = QtGui.QPushButton("Open File")
         open_file.clicked.connect(self.open_file)
+        self.next_channel = QtGui.QPushButton("Next")
+        self.next_channel.clicked.connect(lambda: self.switch_channel(1))
+        self.next_channel.setEnabled(False)
+        self.previous_channel = QtGui.QPushButton("Previous")
+        self.previous_channel.clicked.connect(lambda: self.switch_channel(-1))
+        self.previous_channel.setEnabled(False)
         self.open_label = QtGui.QPushButton("Label")
         self.open_label.clicked.connect(self.label)
         self.open_label.setEnabled(False)
@@ -66,7 +72,7 @@ class PDSViewer(QtGui.QMainWindow):
         quit_button.clicked.connect(self.quit)
 
         horizontal_align.addStretch(1)
-        for button in (open_file, self.open_label, quit_button):
+        for button in (self.previous_channel, self.next_channel, open_file, self.open_label, quit_button):
             horizontal_align.addWidget(button, stretch=0)
 
         hw = QtGui.QWidget()
@@ -80,6 +86,75 @@ class PDSViewer(QtGui.QMainWindow):
         self.setCentralWidget(vw)
         vw.setLayout(vertical_align)
 
+        print sys.argv
+        self.parse_arguments(sys.argv)
+
+    def parse_arguments(self, arguments):
+        total = len(arguments)
+        if total is not 1:
+            for arg in arguments[1:total]:
+                self.names.append(arg)
+            self.name_check()
+            self.index_count = len(self.names)
+            if len(self.names) is not 1 and len(self.names) is not 0:
+                self.next_channel.setEnabled(True)
+                self.previous_channel.setEnabled(True)
+                self.switch_channel(0)
+            elif len(self.names) is 1:
+                self.load_file(self.names[0])
+                self.next_channel.setEnabled(False)
+                self.previous_channel.setEnabled(False)
+
+    def name_check(self):
+        self.verify_names()
+        if(len(self.names) > 1):
+            self.names = self.remove_duplicate_names(self.names)
+        print self.names
+
+    def verify_names(self):
+        """This method removes non-existant files and files with improper file
+        extensions.
+        """
+        removal_flag = 0
+        removal_list = []
+        for name in self.names:
+            if os.path.isfile(name) is not True:
+                print name, "cannot be located. Removing from list..."
+                removal_list.append(name)
+                removal_flag = 1
+            elif name.endswith('.img') is not True and name.endswith('.IMG') is not True:
+                print name, "does not have a valid file extension. Removing from list..."
+                removal_list.append(name)
+                removal_flag = 1
+        if removal_flag is 1:
+            removal_list = self.remove_duplicate_names(removal_list)
+            for name in removal_list:
+                self.names.remove(name)
+            removal_flag = 0
+
+    def remove_duplicate_names(self, name_list):
+        """This function exists to remove duplicate items in lists. It also
+        preserves the order of the list.
+        """
+        seen = {}
+        result = []
+        for item in name_list:
+            marker = item
+            if marker in seen: 
+                print item, "is a duplicate. Removing from list..."
+                continue
+            seen[marker] = 1
+            result.append(item)
+        return result
+
+    def switch_channel(self, direction):
+        self.pos = self.pos + direction
+        if(self.pos > self.index_count):
+            self.pos = 1
+        elif(self.pos < 1):
+            self.pos = self.index_count
+        self.load_file(self.names[self.pos-1])
+
     def label(self):
         # Utilizing the subwindow variables to check if the label window has
         # been opened before. If not, the window is initialized.
@@ -91,14 +166,21 @@ class PDSViewer(QtGui.QMainWindow):
         self._label_window.activateWindow()
 
     def open_file(self):
-        res = QtGui.QFileDialog.getOpenFileName(self, "Open IMG file",
-                                                ".", "IMG files (*.IMG)")
-        if isinstance(res, tuple):
-            file_name = res[0]
-        else:
-            file_name = str(res)
-        if(file_name != ""):
-            self.load_file(file_name)
+        filter = "IMG files (*.IMG)"
+        file_name = QtGui.QFileDialog()
+        file_name.setFileMode(QtGui.QFileDialog.ExistingFiles)
+        opens = file_name.getOpenFileNames(self, "Open IMG files", ".", filter)
+        if(opens[1] != ""):
+            self.names = opens[0]
+            self.index_count = len(self.names)
+            if len(self.names) is not 1:
+                self.next_channel.setEnabled(True)
+                self.previous_channel.setEnabled(True)
+                self.switch_channel(0)
+            else:
+                self.load_file(self.names[0])
+                self.next_channel.setEnabled(False)
+                self.previous_channel.setEnabled(False)
         else:
             print "No file selected!"
             return
@@ -117,10 +199,13 @@ class PDSViewer(QtGui.QMainWindow):
                 self._label_window.label_contents.setText('\n'.join(self.image_label))
                 self._label_window.cancel()
                 self._label_window.show()
+                self._label_window.is_open = True
                 self._label_window.activateWindow()
 
         print('File_Loaded')
         self.setWindowTitle(filepath)
+        # save this line for testing purposes
+        self.loaded_file = os.path.basename(filepath)
         self.open_label.setEnabled(True)
 
     def drop_file(self, pdsimage, paths):
