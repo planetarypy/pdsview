a0255c04827061556d02dbb631ce3267032c2e3d
Perry Vargas <perrybvargas@gmail.com>
2017-04-17 16:50:53 -0700

    Create doc strings, utilize Controllers, make more pythonic with setters

--- pdsview/Histogram.py
+++ pdsview/Histogram.py
@@ -9,67 +9,118 @@ from WarningTimer import WarningTimer, WarningTimerModel
 
 
 class HistogramModel(object):
+    """Model for a Histogram which can apply cut levels to an image
+
+    Any View that utilizes this model must define the following methods:
+    ``set_data``, ``change_cut_low``, ``change_cut_high``, ``change_cuts``,
+    ``warn``, and ``change_bins``. The ``warn`` method must return a boolean
+    and if more than one view utilizes this model, you should consider only
+    one actually creating a warning box and return ``True`` while the others
+    just return ``False``.
+
+    Parameters
+    ----------
+    image_view : :class:`ImageViewCanvas`
+        The image view canvas
+    cut_low ::obj:`float`
+        The lower cut level
+    cut_high : :obj:`float`
+        The higher cut level
+    bins : :obj:`int`
+        The number of bins the histogram uses
+    """
 
     def __init__(self, image_view, cut_low=None, cut_high=None, bins=100):
 
         self._image_view = image_view
-        self._listeners = set()
+        self._views = set()
         self._cut_low = cut_low
         self._cut_high = cut_high
         self._bins = bins
 
     @property
     def image_view(self):
+        """:class:`ImageViewCanvas` The image view canvas
+
+        Setting the image view will reset the data
+        """
         return self._image_view
+
+    @image_view.setter
+    def image_view(self, image_view):
+        """Set the image view
+
+        Parameters
+        ----------
+        image_view : :class:`ImageViewCanvas`
+            The image view canvas
+        """
+        self._image_view = image_view
+        self.set_data()
 
     @property
     def cut_low(self):
+        """:obj:`float` The lower cut level
+
+        Setting the low cut value will adjust the cut values in the image view
+        and notify the views that the low cut value changed
+        """
         return self._cut_low
+
+    @cut_low.setter
+    def cut_low(self, cut_low):
+        self._cut_low = cut_low
+        self._set_view_cuts()
+        self._change_cut_low()
 
     @property
     def cut_high(self):
+        """:obj:`float` The higher cut level
+
+        Setting the high cut value will adjust the cut values in the image view
+        and notify the views that the high cut value changed."""
         return self._cut_high
+
+    @cut_high.setter
+    def cut_high(self, cut_high):
+        self._cut_high = cut_high
+        self._set_view_cuts()
+        self._change_cut_high()
 
     @property
     def bins(self):
+        """:obj:`int` The number of bins the histogram uses
+
+        Setting the bins will notify the views that the bins have changed
+        """
         return self._bins
+
+    @bins.setter
+    def bins(self, bins):
+        if bins == self._bins:
+            return
+        self._bins = bins
+        self._change_bins()
 
     @property
     def cuts(self):
+        """:obj:`tuple` The lower and higher cut levels. If the lower and
+        higher cut levels are not set, use the image_view cut levels
+
+        Setting the cuts will adjust the cut levels in the image viewer and
+        notify the views that the cuts have changed. The low cut must be
+        less than the high cut, otherwise they will be switched to satisfy
+        that condition.
+        """
         if self.cut_low is not None and self.cut_high is not None:
             cut_low, cut_high = self.cut_low, self.cut_high
         else:
             cut_low, cut_high = self.view_cuts
         return cut_low, cut_high
 
-    @property
-    def view_cuts(self):
-        return self.image_view.get_cut_levels()
-
-    @property
-    def data(self):
-        return self.image_view.get_image().data
-
-    def register(self, listener):
-        self._listeners.add(listener)
-
-    def unregister(self, listener):
-        self._listeners.remove(listener)
-
-    def set_image_view(self, image_view):
-        self._image_view = image_view
-
-    def set_cut_low(self, cut_low):
-        self._cut_low = cut_low
-        self._set_view_cuts()
-        self._change_cut_low()
-
-    def set_cut_high(self, cut_high):
-        self._cut_high = cut_high
-        self._set_view_cuts()
-        self._change_cut_high()
-
-    def set_cuts(self, cut_low, cut_high):
+    @cuts.setter
+    def cuts(self, cuts):
+        cut_low, cut_high = cuts
         if cut_low > cut_high:
             message = (
                 "The low cut cannot be bigger than the high cut. " +
@@ -79,68 +130,138 @@ class HistogramModel(object):
 
         diff_cut_low = cut_low != self.cut_low
         diff_cut_high = cut_high != self.cut_high
+
         if diff_cut_low and diff_cut_high:
-            self._cut_low = cut_low
-            self._cut_high = cut_high
+            self._cut_low, self._cut_high = cut_low, cut_high
             self._set_view_cuts()
             self._change_cuts()
         elif diff_cut_low:
-            self.set_cut_low(cut_low)
+            self.cut_low = cut_low
         elif diff_cut_high:
-            self.set_cut_high(cut_high)
+            self.cut_high = cut_high
 
-    def reset_cuts(self):
-        self._cut_low, self._cut_high = self.view_cuts
-        self._change_cuts()
+    @property
+    def view_cuts(self):
+        """:obj:`tuple` The image_view cut levels"""
+        cut_low, cut_high = self.image_view.get_cut_levels()
+        return cut_low, cut_high
 
-    def set_bins(self, bins):
-        if bins == self._bins:
-            return
-        self._bins = bins
-        self._change_bins()
+    @property
+    def data(self):
+        """:class:`ndarray` The current image data"""
+        return self.image_view.get_image().data
+
+    def register(self, view):
+        """Register a view with the model
+
+        Parameters
+        ----------
+        view : :class:`QtGui.QWidget`
+            A view that utilizes this model
+        """
+        self._views.add(view)
+
+    def unregister(self, view):
+        """Unregister a view with the model
+
+        Parameters
+        ----------
+        view : :class:`QtGui.QWidget`
+            A view that utilizes this model
+        """
+        self._views.remove(view)
 
     def set_data(self):
-        for listener in self._listeners:
-            listener.set_data()
+        """Set the data the histogram is to display"""
+        for view in self._views:
+            view.set_data()
 
     def restore(self):
+        """Restore the cut levels"""
         cut_low, cut_high = self.view_cuts
-        self.set_cuts(cut_low, cut_high)
+        self.cuts = cut_low, cut_high
 
     def warn(self, title, message):
+        """Display a warning box
+
+        Each view must define a ``warn`` method that returns a boolean value:
+        True when a warning box is displayed and False when a warning
+        box not displayed. Only one display box will be displayed. This is
+        because multiple views should not have different handling for the same
+        errors.
+        """
         warnings.warn(message)
-        for listener in self._listeners:
-            warned = listener.warn(title, message)
+        for view in self._views:
+            warned = view.warn(title, message)
             if warned:
                 break
 
     def _set_view_cuts(self):
+        """Set the image view cut levels"""
         self.image_view.cut_levels(self.cut_low, self.cut_high)
 
     def _change_cut_low(self):
-        for listener in self._listeners:
-            listener.change_cut_low()
+        """Notfiy the views to that the low cut level was changed"""
+        for view in self._views:
+            view.change_cut_low()
 
     def _change_cut_high(self):
-        for listener in self._listeners:
-            listener.change_cut_high()
+        """Notify the views the high cut level was changed"""
+        for view in self._views:
+            view.change_cut_high()
 
     def _change_cuts(self):
-        for listener in self._listeners:
-            listener.change_cuts()
+        """Notify the views the cut levels were changed"""
+        for view in self._views:
+            view.change_cuts()
 
     def _change_bins(self):
-        for listener in self._listeners:
-            listener.change_bins()
+        """Notify the views the number of bins were changed"""
+        for view in self._views:
+            view.change_bins()
+
+
+class HistogramController(object):
+
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
+
+    def set_cut_low(self, cut_low):
+        self.model.cut_low = cut_low
+
+    def set_cut_high(self, cut_high):
+        self.model.cut_high = cut_high
+
+    def set_cuts(self, cut_low, cut_high):
+        self.model.cuts = cut_low, cut_high
+
+    def set_bins(self, bins):
+        self.model.bins = bins
+
+    def restore(self):
+        self.model.restore()
 
 
 class HistogramWidget(QtGui.QWidget):
+    """View to display the histogram with text boxes for cuts and bins
+
+    Parameters
+    ----------
+    model : :class:`HistogramModel`
+
+    Attributes
+    ----------
+    model : :class:`HistogramModel`
+        The view's model
+    """
 
     def __init__(self, model):
 
         super(HistogramWidget, self).__init__()
         self.model = model
         self.model.register(self)
+        self.controller = HistogramController(self.model, self)
         self._histogram = Histogram(model)
         self._cut_low_label = QtGui.QLabel("Cut Low:")
         self._cut_low_box = QtGui.QLineEdit()
@@ -170,20 +291,25 @@ class HistogramWidget(QtGui.QWidget):
         return layout
 
     def change_cut_low(self):
+        """Set the low cut box text"""
         self._cut_low_box.setText("%.3f" % (self.model.cut_low))
 
     def change_cut_high(self):
+        """Set the high cut box text"""
         self._cut_high_box.setText("%.3f" % (self.model.cut_high))
 
     def change_cuts(self):
+        """Set the low and high cut boxes' text"""
         cut_low, cut_high = self.model.cuts
         self._cut_low_box.setText("%.3f" % (cut_low))
         self._cut_high_box.setText("%.3f" % (cut_high))
 
     def change_bins(self):
+        """Change the bins box text"""
         self._bins_box.setText("%d" % (self.model.bins))
 
     def keyPressEvent(self, event):
+        """When the enter button is pressed, adjust the cut levels and bins"""
         if event.key() in (QtCore.Qt.Key_Return, QtCore.Qt.Key_Enter):
             try:
                 cut_low = float(self._cut_low_box.text())
@@ -211,13 +337,14 @@ class HistogramWidget(QtGui.QWidget):
                     self.change_bins()
                     return
 
-            self.model.set_cuts(cut_low, cut_high)
-            self.model.set_bins(bins)
+            self.controller.set_cuts(cut_low, cut_high)
+            self.controller.set_bins(bins)
 
     # def restore(self):
     #     self.model.restore()
 
     def warn(self, title, message):
+        """Displayed a timed message box the warning"""
         WarningTimer(WarningTimerModel(self, title, message)).exec_()
         return True
 
@@ -226,6 +353,18 @@ class HistogramWidget(QtGui.QWidget):
 
 
 class Histogram(FigureCanvasQTAgg):
+    """The Histogram View
+
+    Parameters
+    ----------
+    model : :class:`HistogramModel`
+        The view's model
+
+    Attributes
+    ----------
+    model : :class:`HistogramModel`
+        The view's model
+    """
 
     def __init__(self, model):
         fig = Figure(figsize=(2, 2), dpi=100)
@@ -236,6 +375,7 @@ class Histogram(FigureCanvasQTAgg):
 
         self.model = model
         self.model.register(self)
+        self.controller = HistogramController(self.model, self)
         self._figure = fig
         policy = self.sizePolicy()
         policy.setHeightForWidth(True)
@@ -247,6 +387,7 @@ class Histogram(FigureCanvasQTAgg):
         self._right_vline = None
 
     def change_cut_low(self, draw=True):
+        """Change the position of the left line to the low cut level"""
         if self._left_vline is None:
             return
         self._left_vline.set_xdata([self.model.cut_low, self.model.cut_low])
@@ -254,6 +395,7 @@ class Histogram(FigureCanvasQTAgg):
             self.draw()
 
     def change_cut_high(self, draw=True):
+        """Change the position of the right line to the high cut level"""
         if self._right_vline is None:
             return
         self._right_vline.set_xdata([self.model.cut_high, self.model.cut_high])
@@ -261,14 +403,24 @@ class Histogram(FigureCanvasQTAgg):
             self.draw()
 
     def change_cuts(self):
+        """Change the position of the left & right lines to respective cuts"""
         self.change_cut_low(draw=False)
         self.change_cut_high(draw=False)
         self.draw()
 
     def change_bins(self):
+        """Adjust the number of bins without adjusting the lines"""
         self.set_data(False)
 
     def set_data(self, reset_vlines=True):
+        """Set the histogram's data
+
+        Parameters
+        ----------
+        reset_vlines : :obj:`bool`
+            Reset the vertical lines to the default cut levels if True,
+            otherwise False. True by default
+        """
         self._ax.cla()
         self._left_vline = None
         self._right_vline = None
@@ -278,18 +430,20 @@ class Histogram(FigureCanvasQTAgg):
         self.draw()
 
     def _move_line(self, event):
+        # The left mouse button must be down to adjust the cut levels
         if not event.inaxes or event.button != 1:
             return
         x = event.xdata
         cut_low, cut_high = self.model.cuts
+        # Adjust the line that is closer to the point
         if np.abs(x - self.model.cut_low) < np.abs(x - self.model.cut_high):
-            self.model.set_cut_low(x)
+            self.controller.set_cut_low(x)
         else:
-            self.model.set_cut_high(x)
+            self.controller.set_cut_high(x)
 
     def _set_vlines(self, reset=True):
         if reset:
-            self.model.reset_cuts()
+            self.model.restore()
         cut_low, cut_high = self.model.cuts
         self._left_vline = self._ax.axvline(
             cut_low, color='r', linewidth=2)
--- pdsview/WarningTimer.py
+++ pdsview/WarningTimer.py
@@ -2,63 +2,164 @@ from ginga.qtw.QtHelp import QtGui, QtCore
 
 
 class WarningTimerModel(object):
+    """Model for Warning Boxes that close after a certain amount of time
+
+    Parameters
+    ----------
+    parent
+        The parent of the QMessageBox
+    title : :obj:`str`
+        The title of the window
+    message : :obj:`str`
+        The warning message
+    time_to_wait : :obj:`int`
+        The amount of time in seconds to wait. 3 seconds by default
+
+    Attributes
+    ----------
+    parent
+        The parent of the QMessageBox
+    """
 
     def __init__(self, parent, title, message, time_to_wait=3):
-        self._listeners = set()
+        self._views = set()
         self.parent = parent
-        self.title = title
-        self.message = message
-        self.time_to_wait = time_to_wait
-        self.text = None
-        self.setText()
+        self._title = title
+        self._message = message
+        self._time_to_wait = time_to_wait
 
-    def register(self, listener):
-        self._listeners.add(listener)
+    @property
+    def title(self):
+        """The warning window title
 
-    def unregister(self, listener):
-        self._listener.remove(listener)
+        Setting the title will notify the views to change the window title
+        """
+        return self._title
 
-    def minusOneSecond(self):
-        self.time_to_wait -= 1
+    @title.setter
+    def title(self, new_title):
+        self._title = new_title
+        for view in self._views:
+            view.change_title()
+
+    @property
+    def message(self):
+        """The warning message to be displayed
+
+        Setting the message will notify the views to change the message
+        """
+        return self._message
+
+    @message.setter
+    def message(self, new_message):
+        self._message = new_message
+        for view in self._views:
+            view.chanage_text()
+
+    @property
+    def time_to_wait(self):
+        """The amount of time in seconds to wait until the window closes
+
+        Setting the time will notify the views to either close if the time is
+        less than 0 or adjust the text to show how much time is left to wait
+        """
+        return self._time_to_wait
+
+    @time_to_wait.setter
+    def time_to_wait(self, time_to_wait):
+        self._time_to_wait = time_to_wait
         if self.time_to_wait < 0:
-            self.closeViews()
+            self.close_views()
         else:
-            self.setText()
+            for view in self._views:
+                view.change_text()
 
-    def closeViews(self):
-        for listener in self._listeners:
-            listener.closeView()
-
-    def setText(self):
+    @property
+    def text(self):
+        """The message with the amount of time until the window closes"""
         time_text = "\n\n Closing in %d seconds" % (self.time_to_wait)
-        self.text = self.message + time_text
-        for listener in self._listeners:
-            listener.changeText()
+        return self.message + time_text
+
+    def register(self, view):
+        """Register a view with the model"""
+        self._views.add(view)
+
+    def unregister(self, view):
+        """Unregister a view with the model"""
+        self._view.remove(view)
+
+    def close_views(self):
+        """Close all the views (called when the time has run out)"""
+        for view in self._views:
+            view.close_view()
+
+
+class WarningTimerController(object):
+    """The Warning Timer Controller
+
+    Parameters
+    ----------
+    model : :class:`WarningTimerModel`
+        The Warning Timer Model
+    view : :class:`QtGui.QMessageBox`
+        A message box with a :class:`WarningTimerModel` model
+    """
+
+    def __init__(self, model, view):
+        self._model = model
+        self._view = view
+
+    def minus_one_second(self):
+        """Subtract a second from the time to wait"""
+        self._model.time_to_wait -= 1
 
 
 class WarningTimer(QtGui.QMessageBox):
+    """A Warning Message Box that closes after some time
+
+    Parameters
+    ----------
+    model : :class:`WarningTimerModel`
+        The Warning Timer Model
+
+    Attributes
+    ----------
+    model : :class:`WarningTimerModel`
+        The Warning Timer Model
+    control : :class:`WarningTimerController`
+        The controller
+    timer : :class:`QtCore.QTimer`
+        The timer
+    """
 
     def __init__(self, model):
         super(WarningTimer, self).__init__(model.parent)
         self.model = model
+        self.control = WarningTimerController(self.model, self)
         self.model.register(self)
-        self.changeText()
+        self.change_text()
         self.timer = QtCore.QTimer(self)
         self.timer.setInterval(1000)
-        self.timer.timeout.connect(self.oneSecondPassed)
+        self.timer.timeout.connect(self.one_second_passed)
         self.timer.start()
         self.setWindowTitle(self.model.title)
         self.setStandardButtons(QtGui.QMessageBox.Ok)
         self.setIcon(QtGui.QMessageBox.Warning)
 
-    def oneSecondPassed(self):
-        self.model.minusOneSecond()
+    def one_second_passed(self):
+        """After a second, subtract from the time_to_wait"""
+        self.control.minus_one_second()
 
-    def changeText(self):
+    def change_text(self):
+        """Set the text to the text given by the model"""
         self.setText(self.model.text)
 
-    def closeView(self):
-        self.timer.stop()
+    def change_title(self):
+        """Change the title to the model's title"""
+        self.setWindowTitle(self.model.title)
+
+    def close_view(self):
+        """Close the Warning Box"""
         self.close()
 
     def closeEvent(self, event):
